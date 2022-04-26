173b58a639551d497e6dd72c9451bb665c9700c6
Napatx28 <icenapat28@gmail.com>
2022-04-27 02:42:36 +0700

    asd

--- src/app.js
+++ src/app.js
@@ -11,12 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:8080'],
-  })
-)
+app.use(cors())
 app.use(authMiddleware)
 
 app.use('/user', userRoute)
